ideak/intel-gpu-tools

commit 5426dc0a889a859db93cd5283cabd9ea6df9d5ec

lib/kms: Skip rather than fail when a suitable plane can't be found
If the system doesn't have the plane the test wants, let's skip the
test rather than fail it.

Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

lib/igt_kms.c
@@ -2035,9 +2035,9 @@ igt_plane_t *igt_pipe_get_plane_type(igt_pipe_t *pipe, int plane_type)
 		break;
 	}
 
-	igt_assert_f(plane_idx >= 0 && plane_idx < pipe->n_planes,
-		"Valid pipe->planes idx not found. plane_idx=%d plane_type=%d n_planes=%d\n",
-		plane_idx, plane_type, pipe->n_planes);
+	igt_require_f(plane_idx >= 0 && plane_idx < pipe->n_planes,
+		      "Valid pipe->planes idx not found. plane_idx=%d plane_type=%d n_planes=%d\n",
+		      plane_idx, plane_type, pipe->n_planes);
 
 	return &pipe->planes[plane_idx];
 }

intel-lgci-fdo-gitlab-mirror/drm.igt-gpu-tools

commit 852c13ddd448fd668c43584ca3ad1aec49c4633c

lib/igt_kms: Add plane type verification in igt_crtc_get_plane_type
Verify plane type matches requested type to avoid returning invalid
planes.

Fixes: 5426dc0a889a ("lib/kms: Skip rather than fail when a suitable plane can't be found")
Cc: Chris Wilson <chris.p.wilson@intel.com>
Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
Cc: Swati Sharma <swati2.sharma@intel.com>
Cc: Karthik B S <karthik.b.s@intel.com>
Signed-off-by: Jing-Ping Jan <jingpingjan@google.com>
Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>

lib/igt_kms.c
@@ -3537,6 +3537,13 @@ igt_plane_t *igt_crtc_get_plane_type(igt_crtc_t *crtc, int plane_type)
 		      "Valid pipe->planes idx not found. plane_idx=%d plane_type=%d n_planes=%d\n",
 		      plane_idx, plane_type, crtc->n_planes);
 
+	igt_require_f(crtc->planes[plane_idx].type == plane_type,
+		      "Unexpected plane type, found plane_idx=%d, "
+		      "type=%d != requested plane_type=%d (%s)\n", plane_idx,
+		      crtc->planes[plane_idx].type,
+		      plane_type,
+		      plane_type == DRM_PLANE_TYPE_PRIMARY ? "PRIMARY" : "CURSOR");
+
 	return &crtc->planes[plane_idx];
 }
 